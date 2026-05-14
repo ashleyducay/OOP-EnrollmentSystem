177d68498c1dcc002f096cbd0312c4e93abc2a36
Your Name <your_email@example.com>
2026-05-15 00:40:30 +0800

    Create tuition payment model

---
+++ src/main/java/org/example/model/TuitionFeePayment.java
@@ -0,0 +1,26 @@
+package org.example.model;
+
+public class TuitionFeePayment {
+    private double balance;
+    private double totalTuitionFee;
+
+    public TuitionFeePayment(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getTotalTuitionFee() {
+        return totalTuitionFee;
+    }
+
+    public void setTotalTuitionFee(double totalTuitionFee) {
+        this.totalTuitionFee = totalTuitionFee;
+    }
+}
