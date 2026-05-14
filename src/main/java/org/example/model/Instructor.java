1f7f73e320e1a220eff0884e284ab5f07dac2af8
Your Name <your_email@example.com>
2026-05-15 00:39:06 +0800

    Add Instructor model extending Person

--- src/main/java/org/example/model/Instructor.java
+++ src/main/java/org/example/model/Instructor.java
@@ -1,31 +1,32 @@
 package org.example.model;
 
 public class Instructor extends Person {
-    private String courses;
+    private String specialization;
 
+    public Instructor() {
+        super();
+    }
 
-    public Instructor(String personID, String personName, String courses){
+    public Instructor(String personName, String personID, String specialization) {
         super(personID, personName);
-        this.courses = courses;
+        this.specialization = specialization;
     }
 
-    public String getCourses() {
-        return courses;
+    public String getSpecialization() {
+        return specialization;
     }
 
-    public void setCourses(String courses) {
-        this.courses = courses;
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
     }
 
-    public void display() {
-        System.out.println("Instructor ID: " + getPersonID());
-        System.out.println("Instructor Name: " + getPersonName());
-        System.out.println("Courses: " + getCourses());
-        System.out.println("---------------------------------");
+    @Override
+    public String getPersonName() {
+        return super.getPersonName();
     }
 
     @Override
     public void mainTask() {
-
+        System.out.println("Instructor is teaching modules.");
     }
 }
