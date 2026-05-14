78c5ad87e13bf0c92a650dbf5c76d71bdbe4cb10
Your Name <your_email@example.com>
2026-05-15 00:40:38 +0800

    Add DepartmentReg service interface

---
+++ src/main/java/org/example/service/DepartmentReg.java
@@ -0,0 +1,11 @@
+package org.example.service;
+import org.example.model.*;
+import java.util.*;
+
+public interface DepartmentReg {
+
+    void save(Department department);
+    List<Department> displayAll();
+
+
+}
