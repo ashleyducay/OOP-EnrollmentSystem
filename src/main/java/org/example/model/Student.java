a2f4c0dbf4ffdc0dcb1dab51bae2bf4b97f5cd8d
Your Name <your_email@example.com>
2026-05-15 00:38:43 +0800

    Add Student model extending Person

--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -3,30 +3,35 @@ package org.example.model;
 public class Student extends Person {
     private String program;
 
-    public Student() {
+    public Student() {}
 
+    public Student(String personID, String personName) {
+        super(personID, personName);
+        this.program = "General";
     }
 
-    public Student(String program){
+
+    public Student(String personID, String personName, String program) {
+        super(personID, personName);
         this.program = program;
+    }
 
-    }
-    public String getProgram (){
-        return program;
+    public String getProgram() { return program; }
+    public void setProgram(String program) { this.program = program; }
 
+    public void display() {
+        System.out.println("Student ID: " + getPersonID());
+        System.out.println("Student Name: " + getPersonName());
+        System.out.println("Program: " + getProgram() + "\n");
     }
-    public void setProgram (String program){
-        this.program = program;
 
-    }
-    public void display(){
-        System.out.println("Student ID: " + " " + getPersonID());
-        System.out.println("Student Name: " + " " + getPersonName());
-        System.out.println("Program: " + " " + getProgram() + "\n");
-    }
     @Override
     public void mainTask() {
         System.out.println("Student Enrolled");
+    }
 
+
+    public TuitionFeePayment getTuitionDetails() {
+        return null;
     }
 }
